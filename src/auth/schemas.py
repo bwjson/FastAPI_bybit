98fdc72850773c26724bf9207983e245b9a46107
bwjson <bwjson27@gmail.com>
2024-09-29 16:35:15 +0500

    auth jwt

---
+++ src/auth/schemas.py
@@ -0,0 +1,21 @@
+from fastapi_users import schemas
+
+class UserRead(schemas.BaseUser[int]):
+	id: int
+	email: str
+	username: str
+	is_active: bool
+	is_superuser: bool
+	is_verified: bool
+
+	class Config:
+		orm_mode = True
+
+class UserCreate(schemas.BaseUser[int]):
+	email: str
+	username: str
+	password: str
+	is_active: bool
+	is_superuser: bool
+	is_verified: bool
+
